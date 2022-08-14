497850b15edb3ad3d3327cafb3ad5927a535e4b1
FELŠÖCI Marek <marek.felsoci@inria.fr>
2022-08-14 20:39:26 +0200

    Manage publishing software environment with Guix

---
+++ manifest.scm
@@ -0,0 +1,13 @@
+(specifications->manifest
+ (list "emacs"
+       "emacs-org"
+       "emacs-org-ref"
+       "emacs-citeproc-el"
+       "emacs-htmlize"
+       "git"
+       "python"
+       "python-pygments"
+       "texlive"
+       "bash"
+       "coreutils"
+       "tar"))
